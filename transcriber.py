015f5edc45a9b674c1fc9a1bbe8621275ac5108d
Jakob Abesser <nudelsalat@posteo.de>
2018-11-20 09:31:37 +0100

    added flex-q-like feature to detect the most likely tatum subdivision per beat

--- transcriber.py
+++ transcriber.py
@@ -65,16 +65,20 @@ class WalkingBassTranscription:
 
     def transcribe(self,
                    fn_wav,
-                   dir_out=None,
+                   dir_out,
                    beat_times=None,
                    tuning_frequency_hz=440.,
-                   threshold=0.2):
+                   threshold=0.2,
+                   aggregation='beat'):
         """ Transcribe audio file
         Args:
             fn_wav (string): WAV file name
             dir_out (string): Directory to store results (if None, same directory as fn_wav is used)
             beat_times (ndarray): Beat times in seconds (if None, only bass saliency is extracted)
             tuning_frequency_hz (float): Tuning frequency (Hz)
+            threshold (float): Decision treshold
+            aggregation (string): Aggregation method. Possible values are 'beat' (beat-wise aggregation)"
+                                  "and 'flex-q' (dynamic estimation of most likely tatum per beat)
         Returns:
             pitch_saliency (2d ndarray): Bass pitch saliency (num_pitches x num_frames)
             midi_axis (ndarray): MIDI pitch values of pitch axis
@@ -115,15 +119,15 @@ class WalkingBassTranscription:
 
         # model prediction
         pitch_saliency = self.model.predict(features)
-        print(pitch_saliency.shape)
-        print(time_axis_sec.shape)
 
+        # save saliency matrix
         base_name = os.path.basename(fn_wav).replace('.wav', '')
         np.save(
             os.path.join(dir_out, '{}_bass_pitch_saliency.npy'.format(base_name)),
             pitch_saliency
         )
 
+        # export most salient bass track as CSV file
         with open(os.path.join(dir_out, '{}_bass_f0.csv'.format(base_name)), 'w') as fhandle:
             writer = csv.writer(fhandle, delimiter=',')
             for t in range(pitch_saliency.shape[0]):
@@ -136,29 +140,197 @@ class WalkingBassTranscription:
 
                 writer.writerow([time_val, freq_val])
 
-        # beat-wise pitch estimation
+        # aggregate pitch saliency to note events
         if beat_times is not None:
-            beat_frames = [closest_bin(time_axis_sec, _) for _ in beat_times]
-            num_beats = len(beat_times)-1
-            beat_bass_pitch = np.zeros(num_beats)
 
-            for b in range(num_beats):
-                beat_pitch_saliency = np.mean(
-                    pitch_saliency[beat_frames[b]: beat_frames[b+1], :], axis=0
-                )
-                best_pitch_idx = np.argmax(beat_pitch_saliency)
-                beat_bass_pitch[b] = self.f_axis_midi[best_pitch_idx]
+            onset_sec, offset_sec, pitch = aggregate_saliency_to_notes(pitch_saliency,
+                                                                       self.f_axis_midi,
+                                                                       time_axis_sec,
+                                                                       beat_times,
+                                                                       method=aggregation,
+                                                                       threshold=threshold)
 
-            score_mat = np.vstack((beat_times[:-1],
-                                   beat_times[1:],
-                                   beat_bass_pitch)).T
-
-            # export note parameters
+            # export score to be imported to Sonic Visualiser as note layer
+            score_mat = np.vstack((onset_sec, offset_sec, pitch)).T
             np.savetxt(os.path.join(dir_out, '{}_bass_line.csv'.format(base_name)),
                        score_mat,
                        fmt='%4.4f,%4.4f,%d')
 
         return pitch_saliency, self.f_axis_midi, time_axis_sec
+
+
+def aggregate_saliency_to_notes(pitch_saliency,
+                                freq_bins_midi,
+                                frame_times_sec,
+                                beat_times_sec,
+                                method='beat',
+                                num_tatums_per_beat=None,
+                                threshold=0.2):
+    """ Aggregate frame-wise pitch saliency values to note events based on given beat times
+    Args:
+        pitch_saliency (2d np.ndarray): Frame-wise pitch saliency (num_frames x num_pitches)
+        freq_bins_midi (np.ndarray): MIDI pitch values (num_pitches)
+        frame_times_sec (np.ndarray): Frame times in seconds (num_pitches)
+        beat_times_sec (np.ndarray): Beat times in seconds (num_beats)
+        method (string): Aggregation method
+        aggregation (string): Aggregation method. Possible values are 'beat' (beat-wise aggregation)"
+                              "and 'flex-q' (dynamic estimation of most likely tatum per beat)
+        num_tatums_per_beat (tuple): Number of tatums per beats - different beat subdivisions that are tested if
+                                     aggregation == 'flex-q' (default: (1, 2, 3))
+        threshold (float): Minimum saliency threshold to detect notes
+    Returns:
+        onset (np.ndarray): Note-wise onset times in seconds
+        offset (np.ndarray): Note-wise offset times in seconds
+        pitch (np.ndarray): Note-wise pitch values
+    """
+    assert method in ('beat', 'flex-q'), "Non-valid value for method!"
+
+    if num_tatums_per_beat is None:
+        num_tatums_per_beat = (1, 3)
+    num_subdivisions = len(num_tatums_per_beat)
+
+    num_beats = len(beat_times_sec)
+    pitch = []
+    onset = []
+    offset = []
+
+    # map beat times from seconds to frames
+    beat_frames = [closest_bin(frame_times_sec, _) for _ in beat_times_sec]
+
+    # iterate over beats
+    for b in range(num_beats-1):
+
+        if method == 'beat':
+            # take most likely pitch
+            beat_pitch_saliency = np.mean(
+                pitch_saliency[beat_frames[b]: beat_frames[b + 1], :], axis=0
+            )
+
+            # if saliency exceeds threshold > store note
+            if np.max(beat_pitch_saliency) > threshold:
+                best_pitch_idx = np.argmax(beat_pitch_saliency)
+                onset.append(beat_times_sec[b])
+                offset.append(beat_times_sec[b+1])
+                pitch.append(freq_bins_midi[best_pitch_idx])
+
+        elif method == 'flex-q':
+
+            curr_onset = []
+            curr_offset = []
+            curr_pitch = []
+
+            scores = np.zeros(num_subdivisions)
+            # try different tatum subdivisions
+            for s, sub_div in enumerate(num_tatums_per_beat):
+
+                scores[s], _, _ = get_sub_beat_saliency(pitch_saliency,
+                                                        beat_times_sec[b],
+                                                        beat_times_sec[b + 1],
+                                                        frame_times_sec,
+                                                        sub_div)
+
+            # get optimal subdivision from highest score
+            sub_div_opt = num_tatums_per_beat[np.argmax(scores)]
+
+            # use optimal subdivision to extract note events
+            _, sub_beat_saliency, sub_beat_times_sec = get_sub_beat_saliency(pitch_saliency,
+                                                                             beat_times_sec[b],
+                                                                             beat_times_sec[b + 1],
+                                                                             frame_times_sec,
+                                                                             sub_div_opt)
+
+            pitch_idx_opt = np.argmax(sub_beat_saliency, axis=1)
+            saliency_opt = np.max(sub_beat_saliency, axis=1)
+
+            # check in which tatum segments, the saliency exceeds the threshold
+            is_valid = saliency_opt >= threshold
+
+            for n in range(sub_div_opt):
+                curr_onset.append(sub_beat_times_sec[n])
+                curr_offset.append(sub_beat_times_sec[n+1])
+                curr_pitch.append(freq_bins_midi[pitch_idx_opt[n]])
+
+            curr_onset = np.array(curr_onset)
+            curr_offset = np.array(curr_offset)
+            curr_pitch = np.array(curr_pitch)
+
+            if np.all(is_valid):
+                # merge adjacent notes with same pitch as we can't do onset detection solely based on saliency
+                if len(np.unique(curr_pitch)) == 1:
+                    curr_pitch = np.array((curr_pitch[0],))
+                    curr_onset = np.array((curr_onset[0],))
+                    curr_offset = np.array((curr_offset[-1],))
+            else:
+                curr_onset = curr_onset[is_valid]
+                curr_offset = curr_offset[is_valid]
+                curr_pitch = curr_pitch[is_valid]
+
+            onset.append(curr_onset)
+            offset.append(curr_offset)
+            pitch.append(curr_pitch)
+
+    if method == 'beat':
+        onset = np.array(onset)
+        offset = np.array(offset)
+        pitch = np.array(pitch)
+    elif method == 'flex-q':
+        onset = np.concatenate(onset)
+        offset = np.concatenate(offset)
+        pitch = np.concatenate(pitch).astype(int)
+
+    return onset, offset, pitch
+
+
+def get_sub_beat_saliency(pitch_saliency,
+                          start_time_sec,
+                          end_time_sec,
+                          frame_times_sec,
+                          sub_div):
+    """ Get pitch saliency and likelihood score for subdivision of given segment in pitch saliency matrix
+    Args:
+        pitch_saliency (2d np.ndarray): Frame-wise pitch saliency (num_frames x num_pitches)
+        start_time_sec (float): Start time in seconds
+        end_time_sec (float): End time in seconds
+        frame_times_sec (np.ndarray): Frame times in seconds
+        sub_div (int, >= 1): Number of subdivisions (e.g. 2 -> given segment is divided into 2 subsegments of equal
+                             duration)
+    Returns:
+        score (float): Likelihood score for current subdivision based on difference between highest and second
+                       highest saliency value
+        sub_beat_saliency (2d np.ndarray): Average pitch saliency vectors for each sub beat (tatum level) (num_sub_beats x num_pitches)
+        sub_beat_times_sec (np.ndarray): Boundary times in seconds for subbeats (num_sub_beats + 1)
+    """
+    num_pitches = pitch_saliency.shape[1]
+    beat_len_sec = end_time_sec - start_time_sec
+    sub_beat_len_sec = beat_len_sec / sub_div
+    # sub_beat times
+    sub_beat_times_sec = np.arange(sub_div + 1) * sub_beat_len_sec + start_time_sec
+    assert len(sub_beat_times_sec) == sub_div + 1
+    # seconds to frames
+    sub_beat_times_frames = [closest_bin(frame_times_sec, _) for _ in sub_beat_times_sec]
+    sub_beat_saliency = np.zeros((sub_div, num_pitches))
+    for sb in range(sub_div):
+        sub_beat_saliency[sb, :] = get_segment_saliency(pitch_saliency,
+                                                        sub_beat_times_frames[sb],
+                                                        sub_beat_times_frames[sb + 1])
+
+    # sort pitch saliency values in descending order accross pitches
+    sub_beat_saliency_sorted = -np.sort(-sub_beat_saliency, axis=1)
+    score = np.mean(sub_beat_saliency_sorted[:, 0] - sub_beat_saliency_sorted[:, 1])
+
+    return score, sub_beat_saliency, sub_beat_times_sec
+
+
+def get_segment_saliency(pitch_saliency, start_frame, end_frame):
+    """ Get average saliency over segment
+    Args:
+        pitch_saliency (2d np.ndarray): Frame-wise pitch saliency (num_frames x num_pitches)
+        start_frame (int): Start frame
+        end_frame (int): End frame
+    Returns:
+        segment_pitch_saliency (np.ndarray): Averaged pitch saliency over segment (num_pitches)
+    """
+    return np.mean(pitch_saliency[start_frame:end_frame, :], axis=0)
 
 
 def closest_bin(axis, val):
@@ -225,7 +397,8 @@ def main(args):
     transcriber.transcribe(args.input_wav,
                            dir_out=args.output_dir,
                            beat_times=beat_times,
-                           threshold=args.threshold)
+                           threshold=args.threshold,
+                           aggregation=args.aggregation)
 
     print('Finished bass line transcription! :)')
 
@@ -250,5 +423,10 @@ if __name__ == '__main__':
                         default=0.2,
                         help="Amplitude threshold. Only used when "
                         "output_format is singlef0 or multif0")
+    parser.add_argument("-a", "--aggregation",
+                        type=str,
+                        default="beat",
+                        help="Aggregation method. Possible values are 'beat' (beat-wise aggregation)"
+                        "and 'flex-q' (dynamic estimation of most likely tatum per beat)")
 
     main(parser.parse_args())
